aee1597495eba773b591734ecc953efe6439bc66
Carlos Augusto Moreno <omorenodovale@gmail.com>
2019-06-25 13:14:15 -0500

    Atualizando configuração no settings.

--- eventex/settings.py
+++ eventex/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 
 # Activate Django-Heroku.
-django_heroku.settings(locals(), test_runner=False)
+django_heroku.settings(locals(), databases=False, test_runner=False)
 
 # Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
